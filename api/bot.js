0a3894f219ea533955e5d031be9a51ba4f95e67f
roshanzameerlohar912-cyber <roshanzameerlohar912@gmail.com>
2026-02-19 21:39:47 +0530

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,22 +1,35 @@
 export default async function handler(req, res) {
+
   const TOKEN = "8320205588:AAG0oUc4psbAOWRqRXZjrFHNKaYhr55dkmY";
   const OWNER_ID = "1187841877";
 
   if (req.method === "POST") {
+
     const data = req.body;
 
+    let messageText = "";
+
+    // Web App se bill aaya
+    if (data.bill) {
+      messageText = data.bill;
+    }
+
+    // Telegram se normal message
     if (data.message) {
-      const text = data.message.text;
-      const chat_id = OWNER_ID;
+      messageText = data.message.text;
+    }
+
+    if (messageText !== "") {
 
       await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          chat_id: chat_id,
-          text: text
+          chat_id: OWNER_ID,
+          text: messageText
         })
       });
+
     }
   }
 
